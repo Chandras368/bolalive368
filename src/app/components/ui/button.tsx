dff9088c741ce131953cd53b663f5cafe5576db0
Chandras368 <chandrasetiawannnn97@gmail.com>
2025-06-09 06:11:53 +0700

    Delete src/app/components/ui directory

--- src/app/components/ui/button.tsx
+++
@@ -1,4 +0,0 @@
-
-export function Button({ children, className = "" }: { children: React.ReactNode, className?: string }) {
-  return <button className={`bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 ${className}`}>{children}</button>;
-}
