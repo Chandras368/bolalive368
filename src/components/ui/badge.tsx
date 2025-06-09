3d84bd3019d850904d998e1efaa804a8bbd1cd8d
Chandras368 <chandrasetiawannnn97@gmail.com>
2025-06-09 16:29:48 +0700

    Delete src directory

--- src/components/ui/badge.tsx
+++
@@ -1,4 +0,0 @@
-
-export function Badge({ children, className = "" }: { children: React.ReactNode, className?: string }) {
-  return <span className={`text-white text-xs font-bold px-2 py-1 rounded ${className}`}>{children}</span>;
-}
